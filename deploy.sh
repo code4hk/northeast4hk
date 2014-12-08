f120eb256b7d1e2ed9767ab4acb0f28a5cd5be18
LCY Vincent <lauchunyin@gmail.com>
2014-12-09 01:46:00 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 git checkout gh-pages
+gulp build
+/bin/cp -fr dist/images .
+/bin/cp -fr dist/scripts .
+/bin/cp -fr dist/styles .
+/bin/cp -fr dist/templates .
+/bin/cp -fr dist/data .
+/bin/cp -fr index.html .
 git merge master
 git add .
 git commit -m 'deploy'
